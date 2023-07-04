05750372daf852a3a8eb548bb60d4b4b3f982bb3
Olga Pomytkina <olpoma@yandex.ru>
2023-07-04 23:15:42 +0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,13 @@ const {
 const NotFoundError = require('../utils/errors/NotFoundError');
 
 // Обработчики POST-запросов signin/signup
-router.post('/api/signin', signinValidator, login);
-router.post('/api/signup', signupValidator, createUser);
+router.post('/signin', signinValidator, login);
+router.post('/signup', signupValidator, createUser);
 
 // руты миддлвэры
 router.use(auth);
-router.use('/api/users', usersRouter);
-router.use('/api/movies', moviesRouter);
+router.use('/users', usersRouter);
+router.use('/movies', moviesRouter);
 
 router.all('/*', (req, res, next) => {
   next(new NotFoundError('Page does not exist'));
