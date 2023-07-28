1ce54d4f87a481bc9b2b44a31f265b7dda34080a
Olga Pomytkina <olpoma@yandex.ru>
2023-07-28 18:41:46 +0700

    fix: mongo url

--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/bitfilmsdb',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:127.0.0.1/bitfilmsdb',
   JWT_SECRET: process.env.JWT_SECRET || '948', // предоставляем безопасное значение по умолчанию
 };
